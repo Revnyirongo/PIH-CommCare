f60e2c4802e2c2f0343c8efc5b9ad33c499fcd83
Revelation <revnyirongo@live.com>
2022-04-12 10:19:59 +0200

    Update export_script.sh
    Multiple config files

--- export_script.sh
+++ export_script.sh
@@ -1,4 +1,4 @@
-for i in 'query1' 'query2' 'query3' 'query4'
+for i in 'query1' 'query2' 'query3' 'query4' /*MUltiple Export Config Files*/
 do
 	echo "Creating export for $i"
 	commcare-export --output-format xlsx --output $i'-export'.xlsx --project <<projectspace>> --query './'$i'.xlsx' 
